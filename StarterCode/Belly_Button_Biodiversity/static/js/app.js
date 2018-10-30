487982772b7ba08b659d2adc16b39ffe5221c6e9
JKaplan814 <j.kaplan814@yahoo.com>
2018-10-29 20:08:48 -0500

    Added ;

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -8,7 +8,7 @@ function buildMetadata(sample) {
       // Use d3 to select the panel with id of `#sample-metadata`
     console.log(data);
 
-    d3.select('#sample-metadata').html("")
+    d3.select('#sample-metadata').html("");
       // Use `.html("") to clear any existing metadata
     Object.entries(data).forEach(([key, value]) => {
       d3.select('#sample-metadata')
